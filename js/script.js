46aae5fa384a4742de88e44eba8d47f686f39448
Daniel <110478467+DanielDiacon@users.noreply.github.com>
2023-06-14 13:33:17 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+const box = document.getElementById('box');
+const widthInput = document.getElementById('widthInput');
+const heightInput = document.getElementById('heightInput');
+const timeInput = document.getElementById('timeInput');
+const colorInput = document.getElementById('colorInput');
+const submitButton = document.getElementById('btn');
+
+const btnStyle = {
+   color: 'white',
+   width: '100px',
+   height: '100px',
+   transition: '1s',
+}
+box.style.backgroundColor = btnStyle.color;
+box.style.width = btnStyle.width;
+box.style.height = btnStyle.height;
+box.style.transition = btnStyle.transition;
+
+submitButton.addEventListener('click', function () {
+   //width
+   btnStyle.width = widthInput.value;
+   if (widthInput.value.includes("px")) {
+      box.style.width = btnStyle.width;
+   } else {
+      box.style.width = btnStyle.width + 'px';
+   }
+
+   //height 
+   btnStyle.height = heightInput.value;
+   if (heightInput.value.includes("px")) {
+      box.style.height = btnStyle.height;
+   } else {
+      box.style.height = btnStyle.height + 'px';
+   }
+
+   //time transition
+   btnStyle.time = timeInput.value;
+   if (timeInput.value == Nan) {
+      box.style.transition = btnStyle.transition;
+   } else if (timeInput.value.includes("s")) {
+      box.style.transition = btnStyle.time;
+   } else {
+      box.style.transition = btnStyle.time + 's';
+   }
+
+   //color
+   btnStyle.color = colorInput.value;
+   box.style.backgroundColor = btnStyle.color;
+});
